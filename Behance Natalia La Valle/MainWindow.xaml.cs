644587ef93f84fb39e49725fc8d8267f6565a227
Kaiser <cristianfiochetti@gmail.com>
2023-04-19 10:31:15 -0300

    Simple changues

---
+++ Behance Natalia La Valle/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using Behance_Natalia_La_Valle.ViewModel;
+
+namespace Behance_Natalia_La_Valle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            juntar = new ClassView();
+            DataContext = juntar;
+            juntar.ValueCanvasRight = 348;
+        }
+        public ClassView juntar;
+        public int X = 0;
+        public Button Btn;
+        private void BtnBranding_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Btn = btn;
+            switch (btn.Name)
+            {
+                case "BtnBranding":
+                    DispatcherTimer Timer = new DispatcherTimer();
+                    Timer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+                    Timer.Tick += (a, b) =>
+                    {
+                        RotateBranding.Angle = RotateBranding.Angle + 4;
+                        if (PT() == BtnPieces)
+                        {
+                            RotateBranding.Angle = 19.820;
+                            Timer.Stop();
+                        }
+
+                    };
+                    Timer.Start();
+                    
+                    break;
+                case "BtnPieces":
+                    DispatcherTimer Timer1 = new DispatcherTimer();
+                    Timer1.Interval = new TimeSpan(0, 0, 0, 0, 1);
+                    Timer1.Tick += (a, b) =>
+                    {
+                        RotatePieces.Angle = RotatePieces.Angle + 4;
+
+                        if (PT() == BtnBranding)
+                        {
+                            RotatePieces.Angle = 361.929; ;
+                            Timer1.Stop();
+                        }
+                    };
+                    Timer1.Start();
+                    break;
+
+
+            }
+        }
+        private void BtnPieces_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (BtnPieces == btn)
+            {
+               
+            }
+        }
+        public Button PT()
+        {
+            return Btn;
+        }
+
+
+        private void BtnPortfolio_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            juntar.MoveRectangle(btn);
+        }
+
+    }
+    
+}
---
+++ Behance Natalia La Valle/ViewModel/ClassView.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Behance_Natalia_La_Valle.ViewModel
+{
+    
+    public class ClassView : INotifyPropertyChanged
+    {
+        public void MoveRectangle(Button button)
+        {
+            if (button.Name == "BtnLanzamientos")
+            {
+                ValueCanvasRight = 232;
+            }
+            if (button.Name == "BtnHablemos")
+            {
+                ValueCanvasRight = 122;
+            }
+            if (button.Name == "BtnPortfolio")
+            {
+                ValueCanvasRight = 348;
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChange(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+        public double valueCanvasRight;
+        public double ValueCanvasRight
+        {
+            get
+            {
+                return valueCanvasRight;
+            }
+            set
+            {
+                valueCanvasRight = value;
+                OnPropertyChange("Move");
+            }
+        }
+        public double Move
+        {
+            get { return valueCanvasRight; }
+        }
+    }
+}
